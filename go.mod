61e009ecd323bf26aa8a8ca28ee158e72eac0fb6
amirdaraby <amir2002.d@gmail.com>
2025-02-08 19:48:50 +0330

    initial: print overall cpu usage in terminal

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amirdaraby/titop
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/amirdaraby/titop/internal/processes"
+
+func main() {
+    processes.CpuOverall()
+}
